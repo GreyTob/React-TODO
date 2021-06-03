9496a87573dff8cc7a4392b9543a33026405192a
Sergey <GreyTob@inbox.ru>
2021-06-03 13:58:18 +0500

    visual

--- src/todo/TodoItem.js
+++ src/todo/TodoItem.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import Context from '../context'
 
@@ -11,35 +11,38 @@ const styles = {
     border: '1px solid #ccc',
     borderRadius: '4px',
     margin: '.5rem',
+    background: 'rgba(100, 100, 130, .5)',
   },
+
   input: {
     marginRight: '1rem',
-  }
+  },
 }
 
-function Todoitem({todo, index, onChange}) {
-
-  const {removeTodo} = useContext(Context)
+function Todoitem({ todo, index, onChange }) {
+  const { removeTodo } = useContext(Context)
 
   const classes = []
   if (todo.completed) {
     classes.push('done')
   }
 
-
-  return(
+  return (
     <li style={styles.li}>
       <span className={classes.join(' ')}>
         <input
           type="checkbox"
           checked={todo.completed}
           style={styles.input}
-          onChange={() => onChange(todo.id)}/>
+          onChange={() => onChange(todo.id)}
+        />
         <strong>{index + 1}</strong>
         &nbsp;
         {todo.title}
       </span>
-      <button className='rm' onClick={() => removeTodo(todo.id)}>&times;</button>
+      <button className="rm" onClick={() => removeTodo(todo.id)}>
+        &times;
+      </button>
     </li>
   )
 }
